d93057dd279f9d0b56d79f7a0f1c5c512a439af0
ohk9412 <85602587+ohk9412@users.noreply.github.com>
2024-08-29 23:50:48 +0900

    Title: 길이 단위 변환하기, Time: 153ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240829/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,7 @@
+let ft = 30.48;
+let mi = 160934;
+
+let a = 9.2
+let b = 1.3
+console.log( `${a}ft = ${( Math.ceil(a*ft * 10) / 10 )}cm` )
+console.log( `${b}mi = ${( Math.ceil(b*mi * 10) / 10 )}cm` )
